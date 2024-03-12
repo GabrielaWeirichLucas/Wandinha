84b5f7e37d46ba4fa88e0295ca97c7fc80cedef2
GabrielaWeirichLucas <162720175+GabrielaWeirichLucas@users.noreply.github.com>
2024-03-12 11:22:53 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://s2-techtudo.glbimg.com/ScdVqz2Hy9EDXy1cO236WyPBV_I=/0x0:1200x700/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/R/k/lsVryvTtAJX1sBtslRxA/wandinha-2.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://muralzinhodeideias.com.br/wp-content/uploads/2022/11/wandinha-divulgacao.jpg";
+}
